24d45687426358f7a125e70cb9561eb9fa8cf71c
myk3l <ayomikesam@gmail.com>
2025-07-29 13:59:51 +0100

    saved

---
+++ app.js
@@ -0,0 +1,22 @@
+// alert("Hello world");
+
+// var userName = 20;
+// console.log(userAge);
+
+var userAge = 29;
+var HeIsActive = true;
+var legnthOfTheRoom = 22;
+console.log(userAge, HeIsActive, legnthOfTheRoom);
+
+var school = {
+  noOfStudent: 20,
+  noOfTeachers: 5,
+  yearsOfEstablishment: 1990,
+  nameOfLocation: "ikeja",
+  nameOfTheSchool: "Sunshine Academy",
+};
+console.log(
+  school.nameOfTheSchool,
+  school.noOfStudent,
+  school.yearsOfEstablishment
+);
--- file.js
+++ file.js
@@ -1,12 +1,19 @@
-// var overlay = document.getElementById("container");
-var add = document.getElementById("event");
-// var button1 = document.getElementById("close");
+// // var overlay = document.getElementById("container");
+// var add = document.getElementById("event");
+// // var button1 = document.getElementById("close");
 
-add.addEventListener("click", () => {
-  events.style.display = "flex";
+// add.addEventListener("click", () => {
+//   events.style.display = "flex";
   
-});
+// });
 
-button1.addEventListener("click", () => {
-  container.style.display = "none";
+// button1.addEventListener("click", () => {
+//   container.style.display = "none";
+// });
+
+var link = document.getElementById("links");
+var menu = document.getElementById("bar");
+
+menu.addEventListener("click", () => {
+  links.style.display = "flex";
 });
