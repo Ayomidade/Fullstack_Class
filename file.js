f93196a20467145d3a4e636f109daf6902b425df
myk3l <ayomikesam@gmail.com>
2025-08-04 13:48:42 +0100

    saved

--- file.js
+++ file.js
@@ -4,16 +4,39 @@
 
 // add.addEventListener("click", () => {
 //   events.style.display = "flex";
-  
+
 // });
 
 // button1.addEventListener("click", () => {
 //   container.style.display = "none";
 // });
 
-var links = document.getElementById("links");
-var menu = document.getElementById("bar");
+// var links = document.getElementById("links");
+// var menu = document.getElementById("bar");
 
-menu.addEventListener("click", () => {
-  links.style.display = "block";
-});
+// menu.addEventListener("click", () => {
+//   links.style.display = "block";
+// });
+
+var isLoggedIn = false;
+if (isLoggedIn) {
+  console.log("You are Active");
+} else if (!isLoggedIn) {
+  console.log("You are Inactive");
+}
+
+var userAge = 10;
+if (userAge >= 50) {
+  console.log("You are old enough");
+}
+else if (userAge <= 45) {
+  console.log("You are young enough");
+} else {
+  console.log("You are not in the age range");
+}
+
+var isLoggedIn = true;
+var isAllowed = false;
+if (isLoggedIn && !isAllowed) {
+  console.log("You are logged in and allowed")
+}
